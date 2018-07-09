2ebadf24b797617f1ae61f0bc5861445516e6f9c
Alexander Gunin <guninalexander@gmail.com>
2018-07-08 23:29:13 -0700

    add files

--- src/align/detect_face.py
+++ src/align/detect_face.py
@@ -191,8 +191,9 @@ class Network(object):
                 print(nodea)
                 nodeb = tf.nn.relu(tf.multiply(neg, tf.nn.max_pool(inp, ksize = [1, 1, 1, 1], strides = [1, 1, 1, 1], padding = 'SAME')))
                 print(nodeb)
-                nodec = tf.multiply(alpha,tf.multiply(neg,
-                                                tf.nn.max_pool(nodeb, ksize = [1, 1, 1, 1], strides = [1, 1, 1, 1], padding = 'SAME')))
+                q = tf.multiply(neg,alpha)
+                print(q)
+                nodec = tf.multiply(tf.nn.max_pool(nodeb, ksize = [1, 1, 1, 1], strides = [1, 1, 1, 1], padding = 'SAME'),q)
                 print(nodec)
             output = tf.add(nodec, nodea)
             if proxy_name is not None:
