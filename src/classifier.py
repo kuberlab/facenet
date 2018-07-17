d2d83c497547d1556915a7bb5cd200be5ae89662
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2018-07-17 13:06:10 +0400

    Upload model only for threshold

--- src/classifier.py
+++ src/classifier.py
@@ -27,10 +27,13 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
+import datetime
 import math
 import os
+from os import path
 import pickle
 import sys
+import shutil
 
 import tensorflow as tf
 import numpy as np
@@ -47,6 +50,20 @@ except ImportError:
 def update_data(data, use_mlboard, mlboard):
     if use_mlboard and mlboard:
         mlboard.update_task_info(data)
+
+
+def upload_model(use_mlboard, mlboard, classifier_path, model, version):
+    if not use_mlboard or not mlboard:
+        return
+
+    print('Uploading model...')
+    dirname = '/tmp/classifier'
+    os.makedirs(dirname, exist_ok=True)
+    shutil.copy(classifier_path, path.join(dirname, path.basename(classifier_path)))
+    mlboard.model_upload(model, version, dirname)
+
+    shutil.rmtree(dirname)
+    print("New model uploaded as '%s', version '%s'." % (model, version))
 
 
 def main(args):
@@ -160,6 +177,17 @@ def main(args):
                 update_data({'accuracy': accuracy}, use_mlboard, mlboard)
                 print('Accuracy: %.3f' % accuracy)
 
+                if args.upload_model and accuracy >= args.upload_threshold:
+                    timestamp = datetime.datetime.now().strftime('%s')
+
+                    upload_model(
+                        use_mlboard,
+                        mlboard,
+                        classifier_filename_exp,
+                        'facenet-classifier',
+                        '1.0.0-%s' % timestamp
+                    )
+
 
 def split_dataset(dataset, min_nrof_images_per_class, nrof_train_images_per_class):
     train_set = []
@@ -177,33 +205,81 @@ def split_dataset(dataset, min_nrof_images_per_class, nrof_train_images_per_clas
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('mode', type=str, choices=['TRAIN', 'CLASSIFY'],
-                        help='Indicates if a new classifier should be trained or a classification ' +
-                             'model should be used for classification', default='CLASSIFY')
-    parser.add_argument('data_dir', type=str,
-                        help='Path to the data directory containing aligned LFW face patches.')
-    parser.add_argument('model', type=str,
-                        help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file')
-    parser.add_argument('classifier_filename',
-                        help='Classifier model file name as a pickle (.pkl) file. ' +
-                             'For training this is the output and for classification this is an input.')
-    parser.add_argument('--use_split_dataset',
-                        help='Indicates that the dataset specified by data_dir should be split into a training and test set. ' +
-                             'Otherwise a separate test set can be specified using the test_data_dir option.',
-                        action='store_true')
-    parser.add_argument('--test_data_dir', type=str,
-                        help='Path to the test data directory containing aligned images used for testing.')
-    parser.add_argument('--batch_size', type=int,
-                        help='Number of images to process in a batch.', default=90)
-    parser.add_argument('--image_size', type=int,
-                        help='Image size (height, width) in pixels.', default=160)
-    parser.add_argument('--seed', type=int,
-                        help='Random seed.', default=666)
-    parser.add_argument('--min_nrof_images_per_class', type=int,
-                        help='Only include classes with at least this number of images in the dataset', default=20)
-    parser.add_argument('--nrof_train_images_per_class', type=int,
-                        help='Use this number of images from each class for training and the rest for testing',
-                        default=10)
+    parser.add_argument(
+        'mode',
+        type=str,
+        choices=['TRAIN', 'CLASSIFY'],
+        help='Indicates if a new classifier should be trained or a classification ' +
+        'model should be used for classification',
+        default='CLASSIFY'
+    )
+    parser.add_argument(
+        'data_dir',
+        type=str,
+        help='Path to the data directory containing aligned LFW face patches.'
+    )
+    parser.add_argument(
+        'model',
+        type=str,
+        help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file'
+    )
+    parser.add_argument(
+        'classifier_filename',
+        help='Classifier model file name as a pickle (.pkl) file. ' +
+        'For training this is the output and for classification this is an input.'
+    )
+    parser.add_argument(
+        '--use_split_dataset',
+        help='Indicates that the dataset specified by data_dir should be split into a training and test set. ' +
+        'Otherwise a separate test set can be specified using the test_data_dir option.',
+        action='store_true'
+    )
+    parser.add_argument(
+        '--test_data_dir',
+        type=str,
+        help='Path to the test data directory containing aligned images used for testing.'
+    )
+    parser.add_argument(
+        '--batch_size',
+        type=int,
+        help='Number of images to process in a batch.',
+        default=90
+    )
+    parser.add_argument(
+        '--image_size',
+        type=int,
+        help='Image size (height, width) in pixels.',
+        default=160
+    )
+    parser.add_argument(
+        '--seed',
+        type=int,
+        help='Random seed.',
+        default=666
+    )
+    parser.add_argument(
+        '--min_nrof_images_per_class',
+        type=int,
+        help='Only include classes with at least this number of images in the dataset',
+        default=20
+    )
+    parser.add_argument(
+        '--nrof_train_images_per_class',
+        type=int,
+        help='Use this number of images from each class for training and the rest for testing',
+        default=10
+    )
+    parser.add_argument(
+        '--upload-threshold',
+        type=int,
+        default=0.9,
+        help='Threshold for uploading model',
+    )
+    parser.add_argument(
+        '--upload-model',
+        type=bool,
+        default=False,
+    )
 
     return parser.parse_args(argv)
 
