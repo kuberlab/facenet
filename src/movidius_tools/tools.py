d382f2696d1863efb4e3b49908917cf69c12f5ee
Alexander Gunin <guninalexander@gmail.com>
2018-07-19 19:05:42 -0700

    onet

--- src/movidius_tools/tools.py
+++ src/movidius_tools/tools.py
@@ -4,7 +4,7 @@ def parse_check_ouput(out):
     for l in out.split('\n'):
         l = l.strip()
         p = l.split(':')
-        if len(p)>0:
+        if len(p)>1:
             if p[0].startswith('Obtained '):
                 name = p[0][len('Obtained '):].strip()
                 value = p[1].strip().rstrip(',')
