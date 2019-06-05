e62d0a7fcce0af575f2071511882f1ecb1888662
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-06-05 14:52:04 +0400

    Don't create bounding box text file

--- src/align/align_dataset_mtcnn.py
+++ src/align/align_dataset_mtcnn.py
@@ -48,6 +48,9 @@ def main(args):
     # Store some git revision info in a text file in the log directory
     src_path, _ = os.path.split(os.path.realpath(__file__))
     facenet.store_revision_info(src_path, output_dir, ' '.join(sys.argv))
+
+    print('Loading list of images to align...')
+
     dataset = facenet.get_dataset(args.input_dir)
 
     print('Creating networks and loading parameters')
@@ -66,86 +69,86 @@ def main(args):
     random_key = np.random.randint(0, high=99999)
     bounding_boxes_filename = os.path.join(output_dir, 'bounding_boxes_%05d.txt' % random_key)
 
-    with open(bounding_boxes_filename, "w") as text_file:
-        nrof_images_total = 0
-        nrof_successfully_aligned = 0
-        if args.random_order:
-            random.shuffle(dataset)
-        for cls in dataset:
-            output_class_dir = os.path.join(output_dir, cls.name)
-            if not os.path.exists(output_class_dir):
-                os.makedirs(output_class_dir)
-                if args.random_order:
-                    random.shuffle(cls.image_paths)
-            for image_path in cls.image_paths:
-                nrof_images_total += 1
-                filename = os.path.splitext(os.path.split(image_path)[1])[0]
-                output_filename = os.path.join(output_class_dir, filename + '.png')
-                print(image_path)
-                if not os.path.exists(output_filename):
+    # with open(bounding_boxes_filename, "w") as text_file:
+    nrof_images_total = 0
+    nrof_successfully_aligned = 0
+    if args.random_order:
+        random.shuffle(dataset)
+    for cls in dataset:
+        output_class_dir = os.path.join(output_dir, cls.name)
+        if not os.path.exists(output_class_dir):
+            os.makedirs(output_class_dir)
+            if args.random_order:
+                random.shuffle(cls.image_paths)
+        for image_path in cls.image_paths:
+            nrof_images_total += 1
+            filename = os.path.splitext(os.path.split(image_path)[1])[0]
+            output_filename = os.path.join(output_class_dir, filename + '.png')
+            print(image_path)
+            if not os.path.exists(output_filename):
+                try:
+                    img = misc.imread(image_path)
+                except (IOError, ValueError, IndexError) as e:
+                    errorMessage = '{}: {}'.format(image_path, e)
+                    print(errorMessage)
+                else:
+                    if img.ndim < 2:
+                        print('Unable to align "%s", shape %s' % (image_path, img.shape))
+                        #text_file.write('%s\n' % output_filename)
+                        continue
+                    if img.ndim == 2:
+                        img = facenet.to_rgb(img)
+                    img = img[:, :, 0:3]
+
                     try:
-                        img = misc.imread(image_path)
-                    except (IOError, ValueError, IndexError) as e:
-                        errorMessage = '{}: {}'.format(image_path, e)
-                        print(errorMessage)
-                    else:
-                        if img.ndim < 2:
-                            print('Unable to align "%s", shape %s' % (image_path, img.shape))
-                            text_file.write('%s\n' % output_filename)
-                            continue
-                        if img.ndim == 2:
-                            img = facenet.to_rgb(img)
-                        img = img[:, :, 0:3]
+                        bounding_boxes, _ = align.detect_face.detect_face_for_align(
+                            img, minsize, pnet, rnet, onet, threshold, factor
+                        )
+                    except Exception as e:
+                        print('Fail to align %s: %s' % (image_path, e))
+                        continue
 
-                        try:
-                            bounding_boxes, _ = align.detect_face.detect_face_for_align(
-                                img, minsize, pnet, rnet, onet, threshold, factor
-                            )
-                        except Exception as e:
-                            print('Fail to align %s: %s' % (image_path, e))
-                            continue
-
-                        nrof_faces = bounding_boxes.shape[0]
-                        if nrof_faces > 0:
-                            det = bounding_boxes[:, 0:4]
-                            det_arr = []
-                            img_size = np.asarray(img.shape)[0:2]
-                            if nrof_faces > 1:
-                                if args.detect_multiple_faces:
-                                    for i in range(nrof_faces):
-                                        det_arr.append(np.squeeze(det[i]))
-                                else:
-                                    bounding_box_size = (det[:, 2] - det[:, 0]) * (det[:, 3] - det[:, 1])
-                                    img_center = img_size / 2
-                                    offsets = np.vstack([(det[:, 0] + det[:, 2]) / 2 - img_center[1],
-                                                         (det[:, 1] + det[:, 3]) / 2 - img_center[0]])
-                                    offset_dist_squared = np.sum(np.power(offsets, 2.0), 0)
-                                    index = np.argmax(
-                                        bounding_box_size - offset_dist_squared * 2.0)  # some extra weight on the centering
-                                    det_arr.append(det[index, :])
+                    nrof_faces = bounding_boxes.shape[0]
+                    if nrof_faces > 0:
+                        det = bounding_boxes[:, 0:4]
+                        det_arr = []
+                        img_size = np.asarray(img.shape)[0:2]
+                        if nrof_faces > 1:
+                            if args.detect_multiple_faces:
+                                for i in range(nrof_faces):
+                                    det_arr.append(np.squeeze(det[i]))
                             else:
-                                det_arr.append(np.squeeze(det))
-
-                            for i, det in enumerate(det_arr):
-                                det = np.squeeze(det)
-                                bb = np.zeros(4, dtype=np.int32)
-                                bb[0] = np.maximum(det[0] - args.margin / 2, 0)
-                                bb[1] = np.maximum(det[1] - args.margin / 2, 0)
-                                bb[2] = np.minimum(det[2] + args.margin / 2, img_size[1])
-                                bb[3] = np.minimum(det[3] + args.margin / 2, img_size[0])
-                                cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
-                                scaled = misc.imresize(cropped, (args.image_size, args.image_size), interp='bilinear')
-                                nrof_successfully_aligned += 1
-                                filename_base, file_extension = os.path.splitext(output_filename)
-                                if args.detect_multiple_faces:
-                                    output_filename_n = "{}_{}{}".format(filename_base, i, file_extension)
-                                else:
-                                    output_filename_n = "{}{}".format(filename_base, file_extension)
-                                misc.imsave(output_filename_n, scaled)
-                                text_file.write('%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
+                                bounding_box_size = (det[:, 2] - det[:, 0]) * (det[:, 3] - det[:, 1])
+                                img_center = img_size / 2
+                                offsets = np.vstack([(det[:, 0] + det[:, 2]) / 2 - img_center[1],
+                                                     (det[:, 1] + det[:, 3]) / 2 - img_center[0]])
+                                offset_dist_squared = np.sum(np.power(offsets, 2.0), 0)
+                                index = np.argmax(
+                                    bounding_box_size - offset_dist_squared * 2.0)  # some extra weight on the centering
+                                det_arr.append(det[index, :])
                         else:
-                            print('Unable to align "%s", n_faces=%s' % (image_path, nrof_faces))
-                            text_file.write('%s\n' % (output_filename))
+                            det_arr.append(np.squeeze(det))
+
+                        for i, det in enumerate(det_arr):
+                            det = np.squeeze(det)
+                            bb = np.zeros(4, dtype=np.int32)
+                            bb[0] = np.maximum(det[0] - args.margin / 2, 0)
+                            bb[1] = np.maximum(det[1] - args.margin / 2, 0)
+                            bb[2] = np.minimum(det[2] + args.margin / 2, img_size[1])
+                            bb[3] = np.minimum(det[3] + args.margin / 2, img_size[0])
+                            cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
+                            scaled = misc.imresize(cropped, (args.image_size, args.image_size), interp='bilinear')
+                            nrof_successfully_aligned += 1
+                            filename_base, file_extension = os.path.splitext(output_filename)
+                            if args.detect_multiple_faces:
+                                output_filename_n = "{}_{}{}".format(filename_base, i, file_extension)
+                            else:
+                                output_filename_n = "{}{}".format(filename_base, file_extension)
+                            misc.imsave(output_filename_n, scaled)
+                            #text_file.write('%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
+                    else:
+                        print('Unable to align "%s", n_faces=%s' % (image_path, nrof_faces))
+                        #text_file.write('%s\n' % (output_filename))
 
     print('Total number of images: %d' % nrof_images_total)
     print('Number of successfully aligned images: %d' % nrof_successfully_aligned)
