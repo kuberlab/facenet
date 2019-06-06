96aa593c390daca209e4c261e5496cbc0feadc99
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-06-06 11:18:13 +0400

    Skip already aligned

--- src/align/align_dataset_mtcnn.py
+++ src/align/align_dataset_mtcnn.py
@@ -87,6 +87,10 @@ def main(args):
             nrof_images_total += 1
             filename = os.path.splitext(os.path.split(image_path)[1])[0]
             output_filename = os.path.join(output_class_dir, filename + '.png')
+            if os.path.exists(output_filename):
+                # Skip
+                print('Skip %s' % image_path)
+                continue
             print(image_path)
             if not os.path.exists(output_filename):
                 try:
